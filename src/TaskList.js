b0736b2b0d3c6f44574f66b4951daa253e5e1a94
Artur Matysiak <inz.matys@gmail.com>
2023-09-01 19:36:11 +0200

    Test

---
+++ src/TaskList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Task from "./Task";
+
+const TaskList = ({ remove, done, TaskList }) => {
+  const showTasks = TaskList.map((task) => (
+    <Task
+      key={task.id}
+      id={task.id}
+      name={task.name}
+      deadline={task.deadline}
+      important={task.important}
+      active={task.active}
+      remove={remove}
+      done={done}
+    />
+  ));
+  return <>{showTasks}</>;
+};
+
+export default TaskList;
